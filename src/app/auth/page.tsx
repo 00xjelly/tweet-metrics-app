78db385445e13b203bac56bc3a45b59ad7f32680
00xjelly <angelomtgarcia@gmail.com>
2025-01-12 19:48:06 +0100

    feat: add auth page

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,9 @@
+import AuthForm from '@/components/AuthForm';
+
+export default function AuthPage() {
+  return (
+    <div className="container mx-auto px-4">
+      <AuthForm />
+    </div>
+  );
+}
