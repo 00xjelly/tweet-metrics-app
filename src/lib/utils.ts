0227c3df4a692fe942c6c4e6ff96ed6f44df3786
00xjelly <angelomtgarcia@gmail.com>
2025-01-22 21:58:06 +0100

    feat: Add tweet ID extraction utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,12 +5,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function formatNumber(num: number): string {
-  if (num >= 1000000) {
-    return (num / 1000000).toFixed(1) + 'M'
+export const extractTweetId = (url: string): string | null => {
+  try {
+    const match = url.match(/\/status\/(\d+)/);
+    return match ? match[1] : null;
+  } catch {
+    return null;
   }
-  if (num >= 1000) {
-    return (num / 1000).toFixed(1) + 'K'
-  }
-  return num.toString()
-}
+};
