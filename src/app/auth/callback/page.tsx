df7e769dfedd516879832b48423fa8ed8a9ac7b5
00xjelly <angelomtgarcia@gmail.com>
2025-01-24 14:45:46 +0100

    Simplify callback page

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,15 +1,6 @@
 'use client'
 
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-
 export default function AuthCallbackPage() {
-  const router = useRouter()
-
-  useEffect(() => {
-    router.push('/')
-  }, [router])
-
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="animate-pulse">Completing sign in...</div>
