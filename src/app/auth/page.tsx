1457dd85701471fee4bbba9d326e24f7ce23aef1
00xjelly <angelomtgarcia@gmail.com>
2025-01-24 15:07:48 +0100

    Delete src/app/auth/page.tsx

--- src/app/auth/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthForm from '@/components/AuthForm';
-
-export default function AuthPage() {
-  return (
-    <div className="container mx-auto px-4">
-      <AuthForm />
-    </div>
-  );
-}
